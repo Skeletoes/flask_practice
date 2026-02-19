3c291e5d742ee071ccc0fe00c3759b6c50a3c317
Skeletoes <23674@burnside.school.nz>
2026-02-19 15:19:38 +1300

    added the pyton code for the flask/sqlite interaction

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, g
+import sqlite3
+
+DATABASE = 'database.db'
 
 app = Flask(__name__)
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 @app.route('/')
 
 def home():
-    return 'Hello'
+    sql = "SELECT * FROM Motorbikes;"
+    results = query_db(sql)
+    return str(results)
 
 if __name__ == '__main__':
     app.run(debug=True)
